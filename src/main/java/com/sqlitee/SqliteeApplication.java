357c6bc39e194ef6a7803a20d937e4a829a3f46a
xiaozhang <xiaozhangqq1@gmail.com>
2017-08-01 11:43:12 +0800

    Initial commit

---
+++ src/main/java/com/sqlitee/SqliteeApplication.java
@@ -0,0 +1,12 @@
+package com.sqlitee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqliteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqliteeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sqlitee/domain/User.java
@@ -0,0 +1,26 @@
+package com.sqlitee.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
